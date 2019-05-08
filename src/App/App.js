a0f323e72db112a4ca23b9d036606a4a55f30b20
Fergus Lemon <fergus.lemon@gmail.com>
2019-05-08 17:35:41 +0100

    Test and update main App component
    Why:
    
    So that it can render child components and have state.
    
    This change addresses the need by:
    
    Updating the component to a class component, rendering the child
    components and passing props to the PriceInput component.

--- src/App/App.js
+++ src/App/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import Header from '../Header/Header.js'
+import PriceInput from '../PriceInput/PriceInput.js'
+import Button from '../Button/Button.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: 1,
+      canSearch: true,
+    }
+  }
+  render() {
+    let { value, canSearch } = this.state
+    return (
+      <div className="App">
+        <div className="App-header">
+          <Header />
+        </div>
+        <div className="Price-input">
+          <PriceInput
+            value={value}
+            handleChange={() => {}}
+            doSearch={() => {}}
+            canSearch={canSearch}
+          />
+        </div>
+        <div className="Submit-button">
+          <Button />
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,9 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import App from './App'
+import { shallow, mount } from 'enzyme'
+import getElement from '../utils/getElement.js'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('The FX Service App', () => {
+  it('renders without crashing', () => {
+    shallow(<App />)
+  })
+
+  it('renders with a Header', () => {
+    const wrapper = mount(<App />)
+    expect(getElement(wrapper)('div')('App-header').text()).toBe(
+      'Forex Rates Service'
+    )
+  })
+  it('renders with an input field', () => {
+    const wrapper = mount(<App />)
+    expect(getElement(wrapper)('input')('price-input-field').exists()).toBe(
+      true
+    )
+  })
+  it('renders with a button', () => {
+    const wrapper = mount(<App />)
+    expect(getElement(wrapper)('button')('submit-rate-request').exists()).toBe(
+      true
+    )
+    expect(getElement(wrapper)('button')('submit-rate-request').text()).toBe(
+      'Submit'
+    )
+  })
+
+  describe('Passing props to child components', () => {
+    it('passes down on props the value, handleChange and doSearch functions and canSearch flag to the PriceInput component', () => {
+      const wrapper = shallow(<App />)
+      const props = wrapper.find('PriceInput').props()
+      const keys = ['value', 'handleChange', 'doSearch', 'canSearch']
+      expect(keys.every(key => key in props)).toBe(true)
+    })
+  })
+
+  describe('Capturing the GBP value entered by a user', () => {
+    describe('When the value is valid', () => {
+      it('stores it on state', () => {
+        const wrapper = shallow(<App />)
+      })
+    })
+  })
+})
