5f5fc81b3324b99fa04adc7e3fd6360ff2108498
Fergus Lemon <fergus.lemon@gmail.com>
2019-05-08 17:29:20 +0100

    Basic Header and Button Components
    Why:
    
    Add a header to the page for structure and a button to kick off a search
    
    This change addresses the need by:
    
    Adding basic Header and Button SFCs and some simple CSS

---
+++ src/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+
+function Header(props) {
+  return (
+    <header className="header">
+      <p className="brand-name">Forex Rates Service</p>
+    </header>
+  )
+}
+
+export default Header
